06eefa52d5f0e2c70e8568bb90a8fefeaba154dd
Juan Camilo Ramirez Cardona <crcamiloramirez@hotmail.com>
2026-07-02 16:22:16 -0500

    fix: webpack-node-externals para que process.env funcione en runtime en Railway

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,7 +1,11 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  target: 'node',
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
   output: {
     path: join(__dirname, '../../dist/apps/api'),
     clean: true,
